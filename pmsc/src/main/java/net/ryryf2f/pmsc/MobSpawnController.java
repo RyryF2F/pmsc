b3fec173881a3892ea727d094bc531a87e42195d
RyryF2F <ryan.butterbrodt@gmail.com>
2023-02-09 13:11:02 -0600

    v0.2.1
    -Condensed Vanilla Mob Spawn Overrides in Config
    -Added Modded Mob Spawn Overrides in Config
    -Fully added in all Spawn Overrides in MobSpawnController

--- pmsc/src/main/java/net/ryryf2f/pmsc/MobSpawnController.java
+++ pmsc/src/main/java/net/ryryf2f/pmsc/MobSpawnController.java
@@ -1,6 +1,6 @@
 package net.ryryf2f.pmsc;
 
-import net.fabricmc.fabric.api.biome.v1.BiomeModification;
+
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
@@ -13,56 +13,46 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.SpawnSettings;
 import net.ryryf2f.pmsc.config.ConfigMain;
 
-import java.util.ArrayList;
-
-
 public class MobSpawnController
 {
+    /**
+     * Checks global Config variables to see if any entity lists need to be generated
+     */
     public static void loadMobSpawns()
     {
         ConfigMain CONFIG = Pmsc.CONFIG;
 
         if (CONFIG.generateAllVanillaMobs)
         {
-            //CONFIG.vanillaMobSpawnsMonster.add(new ConfigMain.CustomMobSpawn(""))
             Pmsc.LOGGER.info("Generation of all vanilla mobs requested, building...");
             Registries.ENTITY_TYPE.forEach(entityType -> {
                 if (getId(entityType).toString().contains("minecraft:"))
-               switch (entityType.getSpawnGroup())
-               {
-                   case MONSTER:
-                       CONFIG.vanillaMobSpawnsMonster.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0, 0, 0,false));
-                       break;
-                   case CREATURE:
-                       CONFIG.vanillaMobSpawnsCreature.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(),0,0,0,false));
-                       break;
-                   case AMBIENT:
-                       CONFIG.vanillaMobSpawnsAmbient.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0,0,0,false));
-                       break;
-                   case WATER_CREATURE:
-                       CONFIG.vanillaMobSpawnsWaterCreature.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0,0,0,false));
-                       break;
-                   case WATER_AMBIENT:
-                       CONFIG.vanillaMobSpawnsWaterAmbient.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0,0,0,false));
-                       break;
-                   case UNDERGROUND_WATER_CREATURE:
-                       CONFIG.vanillaMobSpawnsWaterUnderground.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0,0,0,false));
-                       break;
-                   case AXOLOTLS:
-                       CONFIG.vanillaMobSpawnsAxolotl.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0,0,0,false));
-                       break;
-                   case MISC:
-                       CONFIG.vanillaMobSpawnsMisc.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0,0,0,false));
-                       break;
+                    CONFIG.vanillaMobSpawns.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(),0,0,0,false));
 
-               }
             });
             Pmsc.CONFIG.generateAllVanillaMobs = false;
+            Pmsc.CONFIG.isVanillaGenerated = true;
             Pmsc.LOGGER.info("Vanilla Mob Spawns loaded");
+        }
+
+        if (CONFIG.generateAllModdedMobs)
+        {
+            Pmsc.LOGGER.info("Generation of all modded mobs requested, building...");
+            Registries.ENTITY_TYPE.forEach(entityType ->
+            {
+                if (!getId(entityType).toString().contains("minecraft"))
+                    CONFIG.moddedMobSpawns.add(new ConfigMain.CustomMobSpawn("all", getId(entityType).toString(), entityType.getSpawnGroup(), 0,0,0,false));
+            });
+            Pmsc.CONFIG.generateAllModdedMobs = false;
+            Pmsc.CONFIG.isModdedGenerated = true;
+            Pmsc.LOGGER.info("Modded Mob Spawns Loaded");
         }
     }
 
-
+    /**
+     * Checks if the mod is enabled (CONFIG VALUE)
+     * Then creates BiomeModifications to override spawn settings
+     */
     public static void modifySpawns()
     {
         ConfigMain CONFIG = Pmsc.CONFIG;
@@ -73,10 +63,74 @@ public class MobSpawnController
             Pmsc.LOGGER.info("mod enabled, modifying spawns...");
             if (CONFIG.isVanillaGenerated)
             {
+                Pmsc.LOGGER.info("vanilla mob generation detected, modifying vanilla spawns...");
+                CONFIG.vanillaMobSpawns.forEach(vanillaOverride ->
+                {
+                    if (vanillaOverride.enabled)
+                        if (vanillaOverride.biomeId.equals("all"))
+                        {
+                            BiomeModifications.create(createId(Integer.toString(vanillaOverride.hashCode()))).add(
+                                    ModificationPhase.REPLACEMENTS, BiomeSelectors.all(), context ->
+                                    {
+                                        context.getSpawnSettings().removeSpawnsOfEntityType(Registries.ENTITY_TYPE.get(entityKey(vanillaOverride.mobId)));
 
-            }else
+                                        context.getSpawnSettings().addSpawn(vanillaOverride.spawnGroup, new SpawnSettings.SpawnEntry(Registries.ENTITY_TYPE.get(entityKey(vanillaOverride.mobId)), vanillaOverride.weight, vanillaOverride.minCount, vanillaOverride.maxCount));
+                                    }
+                            );
+                        }else
+                        {
+                            //for specific biomes
+                            BiomeModifications.create(createId(Integer.toString(vanillaOverride.hashCode()))).add(
+                                    ModificationPhase.REPLACEMENTS, BiomeSelectors.includeByKey(biomeKey(vanillaOverride.biomeId)), context ->
+                                    {
+                                        context.getSpawnSettings().removeSpawnsOfEntityType(Registries.ENTITY_TYPE.get(entityKey(vanillaOverride.mobId)));
+                                        context.getSpawnSettings().addSpawn(vanillaOverride.spawnGroup, new SpawnSettings.SpawnEntry(Registries.ENTITY_TYPE.get(entityKey(vanillaOverride.mobId)), vanillaOverride.weight, vanillaOverride.minCount, vanillaOverride.maxCount));
+                                    }
+                            );
+                        }
+                });
+
+            }
+
+            if (CONFIG.isModdedGenerated)
             {
-                CONFIG.mobSpawnsOverride.forEach(mOverride ->
+                Pmsc.LOGGER.info("modded mob generation detected, modifying modded spawns...");
+                CONFIG.moddedMobSpawns.forEach(modOverride ->
+                {
+                    if (modOverride.enabled)
+                        if (modOverride.biomeId.equals("all"))
+                        {
+                            BiomeModifications.create(createId(Integer.toString(modOverride.hashCode()))).add(
+                                    ModificationPhase.REPLACEMENTS, BiomeSelectors.all(), context ->
+                                    {
+                                        context.getSpawnSettings().removeSpawnsOfEntityType(Registries.ENTITY_TYPE.get(entityKey(modOverride.mobId)));
+
+                                        context.getSpawnSettings().addSpawn(modOverride.spawnGroup, new SpawnSettings.SpawnEntry(Registries.ENTITY_TYPE.get(entityKey(modOverride.mobId)), modOverride.weight, modOverride.minCount, modOverride.maxCount));
+                                    }
+                            );
+                        }else
+                        {
+                            //for specific biomes
+                            BiomeModifications.create(createId(Integer.toString(modOverride.hashCode()))).add(
+                                    ModificationPhase.REPLACEMENTS, BiomeSelectors.includeByKey(biomeKey(modOverride.biomeId)), context ->
+                                    {
+                                        context.getSpawnSettings().removeSpawnsOfEntityType(Registries.ENTITY_TYPE.get(entityKey(modOverride.mobId)));
+                                        context.getSpawnSettings().addSpawn(modOverride.spawnGroup, new SpawnSettings.SpawnEntry(Registries.ENTITY_TYPE.get(entityKey(modOverride.mobId)), modOverride.weight, modOverride.minCount, modOverride.maxCount));
+                                    }
+                            );
+                        }
+                });
+
+            }
+
+
+
+
+
+            //RUNNING FINAL OVERRIDES
+            CONFIG.mobSpawnsOverride.forEach(mOverride ->
+            {
+                if (mOverride.enabled)
                 {
                     if (mOverride.biomeId.equals("all"))
                     {
@@ -88,8 +142,7 @@ public class MobSpawnController
                                     context.getSpawnSettings().addSpawn(mOverride.spawnGroup, new SpawnSettings.SpawnEntry(Registries.ENTITY_TYPE.get(entityKey(mOverride.mobId)), mOverride.weight, mOverride.minCount, mOverride.maxCount));
                                 }
                         );
-                    }else
-                    {
+                    } else {
                         //for specific biomes
                         BiomeModifications.create(createId(Integer.toString(mOverride.hashCode()))).add(
                                 ModificationPhase.REPLACEMENTS, BiomeSelectors.includeByKey(biomeKey(mOverride.biomeId)), context ->
@@ -99,18 +152,13 @@ public class MobSpawnController
                                 }
                         );
                     }
-
-                });
-            }
+                }
+            });
         }
-
     }
 
-//    private static RegistryKey<EntityType<?>> entityKey(String id) {
-//        return RegistryKey.of(ENTITY_TYPE, new Identifier(id));
-//    }
-
-    private static Identifier getId(EntityType<?> type) {
+    private static Identifier getId(EntityType<?> type)
+    {
         return Registries.ENTITY_TYPE.getId(type);
     }
 
@@ -124,15 +172,8 @@ public class MobSpawnController
         return new Identifier("pmsc", name);
     }
 
-    private static RegistryKey<Biome> biomeKey(String id) {
+    private static RegistryKey<Biome> biomeKey(String id)
+    {
         return RegistryKey.of(RegistryKeys.BIOME, new Identifier(id));
     }
-
-    private static void clearVanillaMobs()
-    {
-        ConfigMain CONFIG = Pmsc.CONFIG;
-        CONFIG.vanillaMobSpawnsMonster = new ArrayList<>();
-        CONFIG.vanillaMobSpawnsCreature = new ArrayList<>();
-    }
-
 }
